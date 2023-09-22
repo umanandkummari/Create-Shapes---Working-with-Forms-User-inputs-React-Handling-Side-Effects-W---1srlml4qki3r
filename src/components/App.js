8814f2737c7e80420c4096b01bdf3251d6623b11
umanandkummari <116665413+umanandkummari@users.noreply.github.com>
2023-09-23 00:26:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,36 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  const[count, setCount] = useState(0);
+
+
+  
+  
+  const counter =(e)=>{
+    let select = document.getElementById('select') ;
+    let value = select.value ;
+    setCount(count+1);
+    const shapesHolder = document.getElementById('shapes-holder')
+    let el = document.createElement('div');
+    el.classList.add(value.toLowerCase()) ; 
+    el.innerHTML=count;
+    shapesHolder.append(el);
+    
+  }
+
   return (
     <div id="main">
       <div id="shape-creator">
-        
+          <select id='select' >
+            <option value="circle">Circle</option>
+            <option value="square">Square</option>
+          </select>
+
+          <button onClick={counter}>Add Shape</button>
 
       </div>
       <div id="shapes-holder">
+            
       </div>
     </div>
   )
